215165ac232aacc431286f3c988512c78dedb1d9
koog7 <k.ognev709@mail.com>
2024-06-24 11:48:34 +0600

    Добавлена вторая страница About Us , обновлены стили

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import {NavLink, Route, Routes} from "react-router-dom";
 import ContactForm from "./components/ContactForm.tsx";
 import {Avatar, Button} from "@mui/material";
 import SettingsPNG from "./assets/img/Settings-PNG.png";
+import AboutUs from "./components/AboutUs.tsx";
 
 const App: React.FC = () => (
     <>
         <div>
-            <div className="navbar" style={{borderBottom:'1px solid red'}}>
+            <div className="navbar" style={{borderBottom: '1px solid red'}}>
                 <div className={'container'}>
                     <div className={'navbar-block'}>
                         <div className="navbar-logo">
@@ -17,11 +18,11 @@ const App: React.FC = () => (
                         </div>
                         <div className="navbar-links">
                             <NavLink className="nav-link" to="/">Home</NavLink>
-                            <NavLink className="nav-link" to="/contact-form">About</NavLink>
-                            <a>Contact</a>
+                            <NavLink className="nav-link" to="/about">About</NavLink>
+                            <NavLink className="nav-link" to='/contact-form'> Contact</NavLink>
                         </div>
                         <div className="navbar-button">
-                            <Button variant="contained" style={{backgroundColor: '#FF952B', color: 'black'}}><img
+                            <Button variant="contained" style={{backgroundColor: '#F20D0D', color: 'white'}}><img
                                 style={{marginRight: '15px'}} width={'20px'} src={SettingsPNG}
                                 alt={'Settings'}/> Settings</Button>
                             <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg"/>
@@ -32,8 +33,47 @@ const App: React.FC = () => (
         </div>
         <Routes>
             <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<AboutUs/>}/>
             <Route path="/contact-form" element={<ContactForm/>}/>
         </Routes>
+        <div className="footers">
+            <div className='fourcolumns'>
+                <div className='row'>
+                    <div className='cloumnone'>
+                        <h1>The Agency for Movies</h1>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='columntwo'>
+                        <p>BISHKEK</p>
+                        <div>
+                            <p className='underline'>bishkek@example.com</p>
+                            <p>+996 (312) 123-456</p>
+                            <p>123 Primernaya str., Bishkek, Fictional Country 720001</p>
+                        </div>
+                        <p className='underline'>SEE ON MAP</p>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='columnthree'>
+                        <p>ALMATY</p>
+                        <div>
+                            <p className='underline'>kazakhstan@example.com</p>
+                            <p>+7 (701) 234-5678</p>
+                            <p>123 Primernaya str., Almaty, Fictional Country 050000</p>
+                        </div>
+                        <p className='underline'>SEE ON MAP</p>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='columnfour'>
+                        <p>WANT TO BE THE SMARTEST IN YOUR OFFICE?</p>
+                        <p className='underline'>SIGN UP FOR OUR NEWSLETTER</p>
+                        <p>FOLLOW US</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </>
 );
 
--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -1,8 +1,69 @@
-
+import '../App.css'
+import { Box, Typography, Grid } from '@mui/material';
 const AboutUs = () => {
     return (
         <div>
-            
+            <Box sx={{ padding: '20px', marginTop: '100px'}}>
+                <Grid container spacing={2} sx={{ marginBottom: '40px' }}>
+                    <Grid item xs={12} md={6}>
+                        <Typography variant="h4" gutterBottom>
+                            Our <span style={{borderBottom: '2px solid #f20d0d',paddingBottom:'3px'}}>Mission</span>
+                        </Typography>
+                        <Typography variant="body1">
+                            <p>
+                                Our mission is to
+                                deliver <span className={'shadow'}>high-quality products</span> and <span className={'shadow'}>services</span> that meet and
+                                exceed our customers' expectations. We strive to foster <span className={'shadow'}>innovation</span>,
+                                embrace <span className={'shadow'}>change</span>, and continuously improve in all aspects of our business.
+                                Our commitment is to create <span className={'shadow'}>value</span> for our customers, employees, and
+                                community by being a leader in our industry. We believe in maintaining the highest
+                                standards of <span className={'shadow'}>integrity</span>, <span className={'shadow'}>transparency</span>,
+                                and <span className={'shadow'}>accountability</span>.
+                            </p>
+                            <p>
+                                Our team is dedicated to providing exceptional customer service, ensuring that every
+                                interaction with our company is a positive experience. We aim to build lasting
+                                relationships based on <span className={'shadow'}>trust</span> and <span className={'shadow'}>mutual respect</span>. Through our
+                                efforts, we hope to inspire others to pursue <span className={'shadow'}>excellence</span> and to make a
+                                meaningful impact on the world.
+                            </p>
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <img
+                            src="https://img.freepik.com/free-photo/indoor-shot-beautiful-happy-african-american-woman-smiling-cheerfully-keeping-her-arms-folded-relaxing-indoors-after-morning-lectures-university_273609-1270.jpg"
+                            alt="Our Mission" style={{width: '100%'}}/>
+                    </Grid>
+                </Grid>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md={6}>
+                        <img
+                            src="https://media.istockphoto.com/id/1088402018/photo/focus-on-history.jpg?s=612x612&w=0&k=20&c=VeIYDga9xyaRyIcquneZciob4OTIVS0SVj0tufwAF9g="
+                            alt="Our History" style={{width: '100%'}}/>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <Typography variant="h4" gutterBottom>
+                            Our <span style={{borderBottom: '2px solid #f20d0d',paddingBottom:'3px'}}>History</span>
+                        </Typography>
+                        <Typography variant="body1">
+                            <p>
+                            <span className={'shadow'}>Founded in 2001</span>, our company has grown from a small startup to a major
+                                player in the industry. Over the years, we
+                                have <span className={'shadow'}>expanded our product line</span> and entered new markets, always staying true
+                                to our core values of quality and customer satisfaction. Our journey has been one
+                                of <span className={'shadow'}>innovation</span>, <span className={'shadow'}>dedication</span>, and <span className={'shadow'}>hard work</span>.
+                            </p>
+                            <p>
+                                We started with just a handful of employees and a vision to make a difference. Today, we
+                                are proud to have a team of over 500 talented professionals who share our passion and
+                                drive. We have faced numerous challenges along the way, but each one has strengthened
+                                our resolve and helped us grow stronger. Our history is a testament to our resilience
+                                and our unwavering commitment to <span className={'shadow'}>excellence</span>.
+                            </p>
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Box>
         </div>
     );
 };
